898934278fe0d91ee03604ec058df137c8631c03
atsuto0525 <binjiang9673@gmail.com>
2022-10-31 17:53:12 +0800

    Changes

--- o3d_doc_exer/test1.py
+++ o3d_doc_exer/test1.py
@@ -11,3 +11,13 @@ o3d.visualization.draw_geometries([pcd],
                                   front=[0.4257, -0.2125, -0.8795],
                                   lookat=[2.6172, 2.0475, 1.532],
                                   up=[-0.0694, -0.9768, 0.2024])
+
+print("Downsample the point cloud with a voxel of 0.05.")
+downpcd = pcd.voxel_down_sample(voxel_size=0.05)
+print(downpcd)
+o3d.visualization.draw_geometries([downpcd],
+                                  window_name ="voxel down sampled (voxel size = 0.05)",
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
